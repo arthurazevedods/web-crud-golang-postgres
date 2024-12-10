28a86c04014ab6bd17e31bed88aaec34f7ebd7f6
arthur <arthurazevedods@gmail.com>
2024-12-10 11:02:44 -0300

    new product template & route

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -14,3 +14,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	temp.ExecuteTemplate(w, "Index", todosProdutos)
 }
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
